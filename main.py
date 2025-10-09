40a2e60fc957abf67b986d81c99fd779af05d6dd
Prathima Kadari <kadariprathima4@gmail.com>
2025-10-09 23:18:03 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -46,3 +46,82 @@ if add_selectbox == 'Home':
         """,
         unsafe_allow_html=True
     )
+
+    st.subheader('PROBLEM STATEMENT')
+    
+    st.markdown('', 
+         unsafe_allow_html=True)
+
+    
+elif add_selectbox == 'About':
+    
+    st.subheader('ABOUT THE PROJECT')
+
+    st.markdown('<h4>Project Goals</h4>', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    
+    st.markdown('<h4>Developments Made</h4>', unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('•  &nbsp; ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+
+
+elif add_selectbox == 'Features':
+
+    st.subheader('PROJECT ENDORSEMENTS')
+
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+
+elif add_selectbox == 'Car Price by Date':
+
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+
+elif add_selectbox == 'Car Price by Monthly':
+
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+
+elif add_selectbox == 'Visualizations':
+    
+    st.subheader('PROJECT VISUALIZATIONS')
+    st.markdown('<h4>Title1</h4>', unsafe_allow_html=True)
+    st.image(".png", width=400)
+    st.markdown('<h4>Title2</h4>', unsafe_allow_html=True)
+    st.image(".png", width=400)
+    st.markdown('<h4>Title3</h4>', unsafe_allow_html=True)
+    st.image(".png", width=400)
+    st.markdown('<h4>Title4</h4>', unsafe_allow_html=True)
+    st.image(".png", width=500)
+   
+    
+else add_selectbox == 'Conclusion':
+
+    st.subheader('TECH STACK')
+
+    st.markdown('• Data Collection - Google Earth Datasets', unsafe_allow_html=True)
+    st.markdown('• Data Visualization - Google Earth Engine', unsafe_allow_html=True)
+    st.markdown('• Satellite Imagery Analysis - Google Earth Engine', unsafe_allow_html=True) 
+    st.markdown('• Machine Learning - Python, Jupyter Notebooks, Random Forest', unsafe_allow_html=True) 
+    st.markdown('• Dashboard - Streamlit, Heroku', unsafe_allow_html=True) 
+
+    st.subheader('PROJECT SUMMARY')
+
+    st.markdown('', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    
+    st.subheader('CONCLUSION')
+
+    st.markdown('', unsafe_allow_html=True) 
+
+    
